70089fe0842091b4a64076d5995d5ae952ead96e
Michael Standen <screaminghawk@gmail.com>
2023-09-12 20:59:43 +1200

    Add interface docs

--- contracts/interfaces/IOrderbook.sol
+++ contracts/interfaces/IOrderbook.sol
@@ -2,6 +2,18 @@
 pragma solidity ^0.8.4;
 
 interface IOrderbookStorage {
+
+  /**
+   * Order request parameters.
+   * @param isListing True if the order is a listing, false if it is an offer.
+   * @param isERC1155 True if the token is an ERC1155 token, false if it is an ERC721 token.
+   * @param tokenContract The address of the token contract.
+   * @param tokenId The ID of the token.
+   * @param quantity The quantity of tokens.
+   * @param expiry The expiry of the order.
+   * @param currency The address of the currency.
+   * @param pricePerToken The price per token, including royalty fees.
+   */
   struct OrderRequest {
     bool isListing; // True if the order is a listing, false if it is an offer.
     bool isERC1155; // True if the token is an ERC1155 token, false if it is an ERC721 token.
@@ -13,7 +25,18 @@ interface IOrderbookStorage {
     uint256 pricePerToken;
   }
 
-  // Same as above with creator
+  /**
+   * Order parameters.
+   * @param creator The address of the order creator.
+   * @param isListing True if the order is a listing, false if it is an offer.
+   * @param isERC1155 True if the token is an ERC1155 token, false if it is an ERC721 token.
+   * @param tokenContract The address of the token contract.
+   * @param tokenId The ID of the token.
+   * @param quantity The quantity of tokens.
+   * @param expiry The expiry of the order.
+   * @param currency The address of the currency.
+   * @param pricePerToken The price per token, including royalty fees.
+   */
   struct Order {
     address creator;
     bool isListing;
@@ -24,12 +47,6 @@ interface IOrderbookStorage {
     uint96 expiry;
     address currency;
     uint256 pricePerToken;
-  }
-
-  enum TokenType {
-    UNKNOWN,
-    ERC1155,
-    ERC721
   }
 }
 
@@ -128,7 +145,7 @@ interface IOrderbookSignals {
   // Events
   //
 
-  // See IOrderbookFunctions.createOrder
+  /// Emitted when an Order is created.
   event OrderCreated(
     bytes32 indexed orderId,
     address indexed tokenContract,
@@ -140,41 +157,41 @@ interface IOrderbookSignals {
     uint256 expiry
   );
 
-  // See IOrderbookFunctions.acceptOrder
+  /// Emitted when an Order is accepted.
   event OrderAccepted(bytes32 indexed orderId, address indexed buyer, address indexed tokenContract, uint256 quantity);
 
-  // See IOrderbookFunctions.cancelOrder
+  /// Emitted when an Order is cancelled.
   event OrderCancelled(bytes32 indexed orderId, address indexed tokenContract);
 
   //
   // Errors
   //
 
-  // Thrown when the token approval is invalid.
+  /// Thrown when the token approval is invalid.
   error InvalidTokenApproval(address tokenContract, uint256 tokenId, uint256 quantity, address owner);
 
-  // Thrown when the currency approval is invalid.
+  /// Thrown when the currency approval is invalid.
   error InvalidCurrencyApproval(address currency, uint256 quantity, address owner);
 
-  // Thrown when order id is invalid.
+  /// Thrown when order id is invalid.
   error InvalidOrderId(bytes32 orderId);
 
-  // Thrown when the parameters of a batch accept request are invalid.
+  /// Thrown when the parameters of a batch accept request are invalid.
   error InvalidBatchRequest();
 
-  // Thrown when quantity is invalid.
+  /// Thrown when quantity is invalid.
   error InvalidQuantity();
 
-  // Thrown when price is invalid.
+  /// Thrown when price is invalid.
   error InvalidPrice();
 
-  // Thrown when royalty is invalid.
+  /// Thrown when royalty is invalid.
   error InvalidRoyalty();
 
-  // Thrown when expiry is invalid.
+  /// Thrown when expiry is invalid.
   error InvalidExpiry();
 
-  // Thrown when the additional fees are invalid.
+  /// Thrown when the additional fees are invalid.
   error InvalidAdditionalFees();
 }
 
